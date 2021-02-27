a23ece6ca08211955f62c0eed835de3319287790
Samuel68-Pinkx <79759316+Samuel68-Pinkx@users.noreply.github.com>
2021-02-27 17:29:39 +0100

    Create GameSettings.lua

---
+++ GameSettings.lua
@@ -0,0 +1,18 @@
+local game = {
+    ["DragonBall"] = {
+        Title = "Pink X Hub - Dragon Ball Z Final Stand2",
+        Style = 3,
+        SizeX = 500,
+        SizeY = 350,
+        Theme = "PinkX"
+    },
+    ["Testexample"] = {
+        Title = "Pink X Hub - Testexample",
+        Style = 3,
+        SizeX = 500,
+        SizeY = 350,
+        Theme = "PinkX"
+    }
+  }
+
+return game
